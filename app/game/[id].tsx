3944daef370fd833827294920d3a9d61223a645e
Brijen Makwana <brijenma@gmail.com>
2023-09-15 16:22:30 +0530

    game trailers

--- app/game/[id].tsx
+++ app/game/[id].tsx
@@ -3,6 +3,7 @@ import { Dimensions, FlatList } from "react-native";
 import { HtmlText } from "@e-mine/react-native-html-text";
 import { ChevronDown } from "@tamagui/lucide-icons";
 import axios from "axios";
+import { ResizeMode, Video } from "expo-av";
 import { useLocalSearchParams } from "expo-router";
 import {
   Accordion,
@@ -110,6 +111,76 @@ const GameScreenshots = (props) => {
   );
 };
 
+const GameTrailers = (props) => {
+  const { id } = props;
+
+  const [gameTrailers, setGameTrailers] = useState([]);
+
+  const getGameTrailers = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.rawg.io/api/games/${id}/movies`,
+        {
+          params: {
+            key: process.env.EXPO_PUBLIC_API_KEY
+          }
+        }
+      );
+
+      setGameTrailers(response.data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // This code block will always be executed
+    }
+  };
+
+  useEffect(() => {
+    getGameTrailers();
+  }, []);
+
+  if (gameTrailers.length === 0) return;
+
+  return (
+    <YStack space={15}>
+      <H4
+        textTransform="capitalize"
+        color="$blue10Dark"
+      >
+        trailers
+      </H4>
+      <FlatList
+        data={gameTrailers}
+        renderItem={({ item }) => (
+          <Video
+            style={{
+              width: Dimensions.get("window").width,
+              aspectRatio: 16 / 9
+            }}
+            source={{
+              uri: item.data.max
+            }}
+            posterSource={{
+              uri: item.preview
+            }}
+            posterStyle={{
+              resizeMode: "cover"
+            }}
+            usePoster
+            useNativeControls
+            resizeMode={ResizeMode.CONTAIN}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        snapToAlignment="start"
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").width}
+      />
+    </YStack>
+  );
+};
+
 const PCRequirements = (props) => {
   const { platforms } = props;
 
@@ -192,13 +263,14 @@ const Game = () => {
       />
 
       <YStack
-        padding={10}
         space={15}
         marginTop={60}
       >
         <H2 color="$blue10Dark">{game?.name}</H2>
 
         <GameScreenshots id={id} />
+
+        <GameTrailers id={id} />
 
         <H4
           textTransform="capitalize"
