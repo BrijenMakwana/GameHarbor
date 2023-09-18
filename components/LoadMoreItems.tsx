2baab521bffc7690201d3516f702f0ab48a4d926
Brijen Makwana <brijenma@gmail.com>
2023-09-18 10:12:26 +0530

    load more games in game carousel

--- components/LoadMoreItems.tsx
+++ components/LoadMoreItems.tsx
@@ -1,27 +1,31 @@
 import { Loader2 } from "@tamagui/lucide-icons";
-import { Button, Spinner } from "tamagui";
+import { Button, Spinner, View } from "tamagui";
 
 const LoadMoreItems = (props) => {
   const { isLoadingMore, onPress } = props;
 
-  if (isLoadingMore)
-    return (
-      <Spinner
-        size="large"
-        color="$blue10Dark"
-        marginBottom={30}
-      />
-    );
-
   return (
-    <Button
-      icon={Loader2}
-      onPress={onPress}
-      alignSelf="center"
+    <View
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
       marginBottom={30}
     >
-      Load More
-    </Button>
+      {isLoadingMore ? (
+        <Spinner
+          size="large"
+          color="$blue10Dark"
+          marginBottom={30}
+        />
+      ) : (
+        <Button
+          icon={Loader2}
+          onPress={onPress}
+        >
+          Load More
+        </Button>
+      )}
+    </View>
   );
 };
 
