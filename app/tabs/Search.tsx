d422391ecb25424b230e4cac2ca1d7deb2c837e4
Brijen Makwana <brijenma@gmail.com>
2023-09-19 11:22:25 +0530

    SearchBar

--- app/tabs/Search.tsx
+++ app/tabs/Search.tsx
@@ -1,14 +1,59 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import axios from "axios";
+
+import GameCard from "../../components/GameCard";
+import { MyStack } from "../../components/MyStack";
+import SearchBar from "../../components/SearchBar";
 
 const Search = () => {
+  const [searchedGame, setSearchedGame] = useState("");
+  const [games, setGames] = useState([]);
+
+  const clearSearch = () => {
+    setSearchedGame("");
+    setGames([]);
+  };
+
+  const searchGames = async () => {
+    try {
+      const response = await axios.get("https://api.rawg.io/api/games", {
+        params: {
+          key: process.env.EXPO_PUBLIC_API_KEY,
+          search: searchedGame
+        }
+      });
+
+      setGames(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <MyStack>
+      <SearchBar
+        onPress={searchGames}
+        onClear={clearSearch}
+        searchedGame={searchedGame}
+        setSearchedGame={setSearchedGame}
+      />
+
+      <FlatList
+        data={games}
+        renderItem={({ item }) => (
+          <GameCard
+            {...item}
+            fullWidth
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        style={{
+          marginTop: 20
+        }}
+      />
+    </MyStack>
   );
 };
 
 export default Search;
-
-const styles = StyleSheet.create({});
