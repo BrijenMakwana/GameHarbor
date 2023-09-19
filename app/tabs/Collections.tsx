ca1443c39adf9ba3801812d1daaf5316abc48e3c
Brijen Makwana <brijenma@gmail.com>
2023-09-19 10:42:53 +0530

    tabs

---
+++ app/tabs/Collections.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Collections = () => {
+  return (
+    <View>
+      <Text>Collections</Text>
+    </View>
+  );
+};
+
+export default Collections;
+
+const styles = StyleSheet.create({});
